a57f2850c7a4fddc7b8cdf539b80b24624027883
JoaoJV <joao.jaramillo@gmail.com>
2024-01-10 18:46:45 -0500

    Bootstrap the application using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [NavBarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'auto-service';
